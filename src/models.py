4a15573da20974d1057035138b2a0e1fbfe1d11e
Sergi-03 <robrovskyyt@gmail.com>
2025-01-22 19:15:50 +0000

    correciones hechas

--- src/models.py
+++ src/models.py
@@ -1,11 +1,19 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    
 
 class User(Base):
     __tablename__ = 'user'
@@ -13,14 +21,14 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(140), nullable=False)
-    full_name = Column(String(200))
-    profile_picture = Column(String(250))
-    bio = Column(String(500))
     posts = relationship('Post', back_populates='author')
     comments = relationship('Comment', back_populates='author')
-    likes = relationship('Like', back_populates='user')
+    followers = relationship('Follower', foreign_keys=[Follower.user_to_id], backref='followed_user')
+    following = relationship('Follower', foreign_keys=[Follower.user_from_id], backref='follower_user')
+    
 
 
 
@@ -34,7 +42,7 @@ class Post(Base):
     caption = Column(String(500))
     author = relationship('User', back_populates='posts')
     comments = relationship('Comment', back_populates='post')
-    likes = relationship('Like', back_populates='post')
+    media = relationship('Media', back_populates='post')
 
 
 class Comment(Base):
@@ -48,15 +56,15 @@ class Comment(Base):
     post = relationship('Post', back_populates='comments')
     author = relationship('User', back_populates='comments')
 
-class Like(Base):
-    __tablename__ = 'like'
+class Media(Base):
+    __tablename__ = 'media'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    type = Column(Enum('image', 'video', 'audio', name='media_types'), nullable=False)
+    url = Column(String(500), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'))
-    user = relationship('User', back_populates='likes')
-    post = relationship('Post', back_populates='likes')
+    post = relationship('Post', back_populates='media')
 
     
 
